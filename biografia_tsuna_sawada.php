00c159c3f3ad87cc999e22f6276886a4e4a71ce3
stiven escobar galeano <estibenes@gmail.com>
2019-11-17 12:53:14 -0500

    se agrega hoja de estilo css

---
+++ biografia_tsuna_sawada.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+   <title>Biografia de Sawada Tsunayoshi</title>
+</head>
+<body>
+     <h1>Biografia de sawada Tsunayoshi </h1>
+     <h2>
+            Es estudiante de Secundaria y Jefe en formación de la familia Vongola. La razón por la cual Tsuna fue elegido es porque él es el tatara-tatara-tataranieto del primer jefe Vongola,
+            Giotto, quien se había ido a Japón para retirarse. Asimismo, los demás candidatos, por la posición de la cabeza de la Familia Vongola, fallecieron.
+
+            Antes de que Reborn encontrara a Tsuna, Tsuna era conocido como "Tsuna Inútil" o "Perdedor Tsuna" (Dame-Tsuna) por sus bajas calificaciones, actitud miedosa, mala suerte,
+             y la falta de atletismo. Cuando Reborn le dispara con la Bala de la Última Voluntad de la familia Vongola, regresa como un poderoso guerrero medio desnudo (solo tiene su ropa interior puesta)
+             con una llama en su frente y omitiese ojos. Sin embargo, esto solo funciona si tiene algún tipo de arrepentimiento en el momento que Reborn le dispara . Después de cinco minutos,
+              se convierte de nuevo en su yo normal, por lo general avergonzado por el hecho de que él este en público solo con su ropa interior.   
+     </h2>
+
+     <a href="biografia_tsuna_sawada.php"><img src="imagenes/tsuna.png"></a>
+     <a href="index.php">Volver</a>
+     
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+   Hola Mundo !
+</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+    <!-- Esto es un comentario-->
+    <h1> Hola mundo, me llamo anacleto !</h1>
+    <h2>Subtitulo</h2>
+    <h3>Contenido de informacion</h3>
+    <h4>un texto pequeño</h4>
+    <h5>un texto mas pequeño</h5>
+    <h6>un texto mucho mas pequeño</h6>
+    <p>este es un parrafo para colocar mucha mas informacion</p>
+    <p>tengo un texto con estilo <b>negrita</b></p>
+    <a href="http://www.google.com" target="_blank">click aqui para ir a google</a>
+    <br>
+     hola mundo 2.
+    <br>
+    este es otro texto
+    <br>
+    
+    <a href="biografia_tsuna_sawada.php"><img src="imagenes/tsunahyper.jpg"></a>
+
+</body>
+</html>
